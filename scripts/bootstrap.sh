64a5c917927c461f34d7d5b91d0c4e41d7633700
tsunyoku <tsunyoku@gmail.com>
2024-05-26 15:33:12 +0100

    1 replica, inject secrets

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# catch vault secrets
-if [ -n "$KUBERNETES" ]; then
-  source /vault/secrets/secrets.txt
+if [ -z "$APP_ENV" ]; then
+  echo "Please set APP_ENV"
+  exit 1
 fi
 
+if [[ $PULL_SECRETS_FROM_VAULT -eq 1 ]]; then
+  echo "Fetching secrets from vault"
+  # TODO: revert to $APP_ENV
+  akatsuki vault get beatmaps-service production-k8s -o .env
+  echo "Fetched secrets from vault"
+  source .env
+  echo "Sourced secrets from vault"
+fi
 
 cd /app
 dotnet BeatmapsService.dll
